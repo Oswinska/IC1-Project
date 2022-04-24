cfc7f8ca7183a2fa3837b52553ef2edacc73b447
Mo1ty <81764351+Mo1ty@users.noreply.github.com>
2022-04-24 03:06:27 +0200

    Update App.c. added third vulnerability

--- C/App.c
+++ C/App.c
@@ -15,53 +15,19 @@ typedef struct {
     short status;
 }car;
 
-void rent_car(car* car, customer* client, int time) {
-    printf("Debt now is: %d\n", client->debt);
-    client->debt += car->price_per_hour * time;
-    printf("Debt now is: %d\n", client->debt);
-}
-
-
-void change_number(car* automobile) {
-    printf("Type new number. Only 8 symbols!\n");
-    char *newNumber;
-    printf("Step0 succ!\n");
-    scanf("%s", newNumber);
-    printf("Step1 succ\n");
-    strcpy(automobile->number, newNumber);
-    printf("Step2 succ\n");
-}
-
-void save_file(car car, customer client) {
-    FILE* save_file = fopen("backup.txt", "rw+");
-}
-
-void open_file() {
-    FILE* file;
-    char* line;
-
-    if ((file = fopen("backup.txt", "r+")) == NULL) {
-        printf("cannot open file.\\n");
-        exit(1);
-    };
-    for (int i = 0; i < 3; i++) {
-        fscanf(file, "%s", line);
-
-    };
-}
-
 int menu()
 {
     int command = 0;
-    printf("Welcome to the Car Service! Write the digit: \n 1 - Rent a car. \n 2 - Change name.\n ");
-    printf("3 - Change surname.\n 4 - Save changes.\n 5 - Exit the program.\n");
+    printf("Welcome to the Car Service! Write the digit: \n 1 - Rent a car. \n 2 - Change car number.\n ");
+    printf("3 - Save changes to the file.\n 4 - Find backup file if lost.\n 5 - Exit the program.\n");
     while (command < 1 || command > 4)
     {
         command = 10;
         scanf("%d", &command);
-        if (command >= 1 && command <= 4)
+        if (command >= 1 && command <= 5)
             return command;
         printf("You have written wrong digit. Please, write another one from the list.\n");
+	exit(0);
     }
 }
 
@@ -74,8 +40,7 @@ int op_choose()
     cust.debt = 64000;
 
     car cars;
-    char *newName = "AN9728U";
-    memcpy(cars.number, newName, strlen(newName));
+    strcpy(cars.number, "AN9728U");
     cars.price_per_hour = 700;
     cars.status = 0;
 
@@ -91,7 +56,8 @@ int op_choose()
         printf("For how many hours do you want to rent?\n");
         int val;
         scanf("%d", &val);
-        rent_car(&cars, &cust, val);
+	printf("Debt now is: %d\n", cust.debt);
+	cust.debt += cars.price_per_hour * val;
 	printf("Debt is %d\n", cust.debt);
 	break;
 
@@ -100,20 +66,70 @@ int op_choose()
     case 2:
     {
 	printf("Number is: %s\n", cars.number);
-        change_number(&cars);
+	printf("Price is: %d\n", cars.price_per_hour);
+	
+	printf("Type new number. Only 8 symbols!\n");
+        char *newnum;
+        printf("Step0 succ!\n");
+        scanf("%s", &newnum);
+
+        printf("Step1 succ!\n");
+        memcpy(cars.number, &newnum, sizeof(newnum));
+        printf("Step2 succ!\n");
+
         printf("Number is: %s\n", cars.number);
+	printf("Price is: %d\n", cars.price_per_hour);
         break;
     }
 
     case 3:
     {
 
+	getchar();
+
+	char *path = (char*) malloc(255);
+	printf("Write the path where you want to save your file:\n");
+        scanf("%s[^\n]", path);
+        printf("Path achieved! It is: %s\n", path);
+	
+	char data[255] = { };
+        snprintf(data, sizeof(data), "%s %s %s %hu \n%s %hu %hi", cust.name, cust.surname, cust.account_number, cust.debt, cars.number, cars.price_per_hour, cars.status);
+
+	getchar();
+	char prefix[255];
+        printf("Please, write description about backup.");
+	scanf("%[^\n]s", prefix);
+        printf("Description achieved! It is: %s\n", prefix);
+	char prefix_save[sizeof(path)+sizeof(prefix)+8];
+	snprintf(prefix_save, 272, "echo %s > %s", prefix, path);
+	system(prefix_save);
+
+        printf ("%s\n\n", data);
         printf("Saving the file...\n\n");
+        char to_save[sizeof(path)+sizeof(data)+17];
+        snprintf(to_save, 272, "echo \"%s\" >> %s", data, path);
+        printf("to_save var created! It looks like: %s\n", to_save);
+        system(to_save);
+        printf("File saved successfully\n");
+       
 	break;
 
     }
 
     case 4:
+    {
+    	printf("Input path you are searching file in: \n");
+	getchar();
+        char path[255];
+        scanf("%[^\n]s", path);
+        printf("Path achieved! It is: %s\n", path);
+	char to_save[sizeof(path)+17];
+        snprintf(to_save, sizeof(to_save), "find %s", path);
+	system(to_save);
+	break;
+    }
+
+    case 5:
     {
 
         printf("Exiting...");
